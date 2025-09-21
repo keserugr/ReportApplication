85902c1d0158dfa7bc5db54ae5f328518954ffd1
ugur <keserugr@gmail.com>
2025-09-22 00:05:10 +0100

    created client service and test files

--- src/main/java/com/keserugr/transaction/service/ClientService.java
+++ src/main/java/com/keserugr/transaction/service/ClientService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ClientService {
-
     private final TokenService tokenService;
     private final ApiClient apiClient;
 
--- src/test/java/com/keserugr/transaction/service/ClientServiceTest.java
+++ src/test/java/com/keserugr/transaction/service/ClientServiceTest.java
@@ -88,5 +88,4 @@ class ClientServiceTest {
         //then
         assertEquals("N/A", email);
     }
-
 }
