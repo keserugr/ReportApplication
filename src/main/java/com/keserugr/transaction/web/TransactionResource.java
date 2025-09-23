a1b9c61eefe54439343b9dc7e890fc7008a2bcb7
ugur <keserugr@gmail.com>
2025-09-23 02:22:04 +0100

    some bugs have been fixed.

--- src/main/java/com/keserugr/transaction/web/TransactionResource.java
+++ src/main/java/com/keserugr/transaction/web/TransactionResource.java
@@ -1,5 +1,7 @@
 package com.keserugr.transaction.web;
 
+import com.keserugr.transaction.dto.report.TransactionsReportRequest;
+import com.keserugr.transaction.dto.report.TransactionsReportResponse;
 import com.keserugr.transaction.dto.transaction.GetTransactionResponse;
 import com.keserugr.transaction.dto.transaction.TransactionListRequest;
 import com.keserugr.transaction.dto.transaction.TransactionListResponse;
@@ -18,17 +20,26 @@ public class TransactionResource {
 
     private final TransactionService transactionService;
 
-    @PostMapping("/search")
+    @PostMapping("/report")
+    public ResponseEntity<TransactionsReportResponse> createReport(
+            @Valid @RequestBody TransactionsReportRequest request) {
+        if (request.getToDate().isBefore(request.getFromDate())) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(transactionService.getTransactionReport(request));
+    }
+
+    @PostMapping("/list")
     public ResponseEntity<TransactionListResponse> search(@Valid @RequestBody TransactionListRequest req) {
 
         if (req.getFromDate() != null && req.getToDate() != null
                 && req.getToDate().isBefore(req.getFromDate())) {
             return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.ok(transactionService.searchTransactions(req));
+        return ResponseEntity.ok(transactionService.getTransactionList(req));
     }
 
-    @GetMapping("/{transactionId}/status")
+    @GetMapping("/{transactionId}")
     public ResponseEntity<String> getTransactionStatus(@PathVariable String transactionId) {
         GetTransactionResponse resp = transactionService.getTransactionById(transactionId);
         String status = transactionService.safeStatus(resp);
--- src/test/java/com/keserugr/transaction/web/TransactionResourceTest.java
+++ src/test/java/com/keserugr/transaction/web/TransactionResourceTest.java
@@ -1,11 +1,12 @@
 package com.keserugr.transaction.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.keserugr.transaction.dto.transaction.GetTransactionResponse;
-import com.keserugr.transaction.dto.transaction.TransactionListRequest;
-import com.keserugr.transaction.dto.transaction.TransactionListResponse;
+import com.keserugr.transaction.dto.report.TransactionsReportRequest;
+import com.keserugr.transaction.dto.report.TransactionsReportResponse;
+import com.keserugr.transaction.dto.transaction.*;
 import com.keserugr.transaction.service.TransactionService;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
@@ -14,6 +15,8 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -31,41 +34,44 @@ class TransactionResourceTest {
     @MockitoBean
     TransactionService transactionService;
 
+    //Transactions Report Test
     @Test
-    void search_return_200_when_Dates_valid() throws Exception {
-        //given
-        var request = TransactionListRequest.builder()
+    void create_report_returns_200_when_dates_valid() throws Exception {
+        // given
+        var request = TransactionsReportRequest.builder()
                 .fromDate(LocalDate.parse("2015-07-01"))
                 .toDate(LocalDate.parse("2015-10-01"))
-                .merchantId(1)
+                .merchant(1)
                 .build();
 
-        var response = new TransactionListResponse();
-        response.setCurrent_page(1);
+        var response = new TransactionsReportResponse();
+        response.setStatus("APPROVED");
+        when(transactionService.getTransactionReport(any())).thenReturn(response);
 
-        when(transactionService.searchTransactions(any())).thenReturn(response);
-
-        //when & then
-        mvc.perform(post("/api/v1/transactions/search")
+        // when & then
+        mvc.perform(post("/api/v1/transactions/report")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.current_page").value(1));
+                .andExpect(jsonPath("$.status", is("APPROVED")));
 
-        verify(transactionService, times(1)).searchTransactions(any());
+        ArgumentCaptor<TransactionsReportRequest> captor = ArgumentCaptor.forClass(TransactionsReportRequest.class);
+        verify(transactionService).getTransactionReport(captor.capture());
+        var passed = captor.getValue();
+
+        assertEquals(LocalDate.parse("2015-07-01"), passed.getFromDate());
+        assertEquals(LocalDate.parse("2015-10-01"), passed.getToDate());
     }
 
     @Test
-    void search_return_400_when_toDate_before_fromDate() throws Exception {
-        //given
-        var bad = TransactionListRequest.builder()
+    void create_report_returns_400_when_toDate_before_fromDate() throws Exception {
+        var bad = TransactionsReportRequest.builder()
                 .fromDate(LocalDate.parse("2015-10-01"))
                 .toDate(LocalDate.parse("2015-07-01"))
-                .merchantId(1)
+                .merchant(1)
                 .build();
 
-        //when & then
-        mvc.perform(post("/api/v1/transactions/search")
+        mvc.perform(post("/api/v1/transactions/report")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(bad)))
                 .andExpect(status().isBadRequest());
@@ -74,7 +80,82 @@ class TransactionResourceTest {
     }
 
     @Test
-    void getTransactionStatus_returns200_andStatusFromService() throws Exception {
+    void createReport_returns_400_when_validation_fails() throws Exception {
+        var invalidJson = "{}";
+
+        mvc.perform(post("/api/v1/transactions/report")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(invalidJson))
+                .andExpect(status().isBadRequest());
+
+        verifyNoInteractions(transactionService);
+    }
+
+    //Transaction List Tests
+    @Test
+    void transaction_list_return_200_when_dates_valid() throws Exception {
+        //given
+        var request = TransactionListRequest.builder()
+                .fromDate(LocalDate.parse("2015-07-01"))
+                .toDate(LocalDate.parse("2015-10-01"))
+                .merchantId(1)
+                .build();
+
+        var response = new TransactionListResponse();
+        response.setCurrentPage(1);
+
+        when(transactionService.getTransactionList(any())).thenReturn(response);
+
+        //when & then
+        mvc.perform(post("/api/v1/transactions/list")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.current_page").value(1));
+
+        verify(transactionService, times(1)).getTransactionList(any());
+    }
+
+    @Test
+    void transaction_list_return_200_when_empty_request_was_sent() throws Exception {
+        // given
+        var request = new TransactionListRequest();
+
+        var response = new TransactionListResponse();
+        response.setCurrentPage(3);
+        when(transactionService.getTransactionList(any())).thenReturn(response);
+
+        // when & then
+        mvc.perform(post("/api/v1/transactions/list")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.current_page").value(3));
+
+        verify(transactionService, times(1)).getTransactionList(any());
+    }
+
+    @Test
+    void transaction_list_return_400_when_toDate_before_fromDate() throws Exception {
+        //given
+        var bad = TransactionListRequest.builder()
+                .fromDate(LocalDate.parse("2015-10-01"))
+                .toDate(LocalDate.parse("2015-07-01"))
+                .merchantId(1)
+                .build();
+
+        //when & then
+        mvc.perform(post("/api/v1/transactions/list")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(bad)))
+                .andExpect(status().isBadRequest());
+
+        verifyNoInteractions(transactionService);
+    }
+
+    //Transaction_ById Tests
+    @Test
+    void get_transaction_by_id_returns_200_and_status_from_service() throws Exception {
         //given
         String transactionId = "1-1444392550-1";
 
@@ -84,7 +165,7 @@ class TransactionResourceTest {
         when(transactionService.safeStatus(response)).thenReturn("APPROVED");
 
         //when & then
-        mvc.perform(get("/api/v1/transactions/{transactionId}/status", transactionId))
+        mvc.perform(get("/api/v1/transactions/{transactionId}", transactionId))
                 .andExpect(status().isOk())
                 .andExpect(content().string("APPROVED"));
 
